0384455ee787a5d00fadaeb48c789f13e7c596f6
igorgoc <60901570+igorgoc@users.noreply.github.com>
2026-09-26 16:13:18 +0200

    Increase max open files limit to 1800

--- src/catapult/cache_db/RocksDatabase.cpp
+++ src/catapult/cache_db/RocksDatabase.cpp
@@ -91,7 +91,7 @@ namespace catapult { namespace cache {
 		rocksdb::Options dbOptions;
 		dbOptions.create_if_missing = true;
 		dbOptions.create_missing_column_families = true;
-		dbOptions.max_open_files = 1024;
+		dbOptions.max_open_files = 1800;
 
 		rocksdb::ColumnFamilyOptions defaultColumnOptions;
 		defaultColumnOptions.compaction_filter = m_pruningFilter.compactionFilter();
